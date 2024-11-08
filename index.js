4b32b2ab67ea3a31f4948f8b6db2672d4ba8ac76
omar faruk <farukomarcpa@gmail.com>
2024-11-08 23:01:58 +0600

    mogodb link updated

--- index.js
+++ index.js
@@ -2,17 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
-
 const app=express();
 const port =process.env.PORT||5000;
-console.log(process.env.DB_USER_PORTFOLIO);
+
 //middleware
-app.use(cors());
+const corsOptions = {
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://portfolio12-b6ce8.web.app',
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
 
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER_PORTFOLIO}:${process.env.DB_USER_PASS}@cluster0.2lcaz14.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER_PORTFOLIO}:${process.env.DB_USER_PASS}@cluster0.2lcaz14.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,20 +35,16 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-const messageCollection = client.db('usermessage').collection('messageme');
+const messageCollection = client.db('PortfolioWebsite').collection('userMessage');
+
     app.post('/message',async(req,res)=>{
         const message=req.body;
         const result=await messageCollection.insertOne(message)
         res.send(result);
         
     })
-    // Connect the client to the server	(optional starting in v4.7)
-   
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
   } finally {
-    // Ensures that the client will close when you finish/error
 
   }
 }
@@ -49,5 +55,5 @@ app.get('/',(req,res)=>{
     res.send('portfolio is ready')
 })
 app.listen(port,()=>{
-    console.log('Portfolio is ready for job hunting',`${port}`);
+    console.log(`Portfolio is ready for job hunting,${port}`);
 })
